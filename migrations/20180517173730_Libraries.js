7e2c9ad2fd15bc6274a4e69eb46b720640a4dfe9
Vision-X <michael.s.marlow@gmail.com>
2018-05-17 17:42:33 -0600

    add create and drop table migrations for each of the buckets

---
+++ migrations/20180517173730_Libraries.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('Libraries', table=> {
+    table.increments('id').primary()
+    table.text('title')
+    table.text('body')
+    table.text('link')
+    table.text('tags')
+    table.text('notes')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.scheme.dropTableIfExists('Libraries')
+};
